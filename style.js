b1a4651fa0d6de75d506c980324f87132fcff520
hedgehogua <hedgehog684@gmail.com>
2014-07-14 20:34:14 +0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name 	x
+// @namespace 	x
+// @description x
+// @version     latest
+// @date 	2014-07-14
+// @creator hedgehog684@gmail.com
+// @icon http://xwab.ru/icon.png
+// @include	http://xwab.ru/*
+// @homepage	http://v.ru
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = '//';
+document.body.appendChild(script);
