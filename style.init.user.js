295b84b41a05625520987cda55e17f2628049222
hedgehogua <hedgehog684@gmail.com>
2014-07-14 20:44:47 +0300

    Update style.init.user.js

--- style.init.user.js
+++ style.init.user.js
@@ -2,10 +2,9 @@
 // @name 	xWab.RU BlackOrange Style Helper
 // @namespace 	http://xwab.ru
 // @description Blackorange Style Helper
-// @version     latets
+// @version     0.03
 // @date 	2014-07-14
 // @creator hedgehog684@gmail.com
-// @icon http://xwab.ru/icon.png
 // @include	http://xwab.ru/*
 // @homepage	http://xwab.ru
 // ==/UserScript==
